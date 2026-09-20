8ddff56405002510c135dd4b0ba17a3b7696433b
munawir <munnumunawir0@gmail.com>
2026-09-20 13:09:26 +0400

    Prepare frontend for production deployment

--- src/DestinationCard.jsx
+++
@@ -1,9 +0,0 @@
-function DestinationCard(props){
-return(
-    <div className="destination-card">
-<h2>{props.destination}</h2>
-<p>Explore the beautifull city of{props.destination}</p>
-</div>
-)
-}
-export default DestinationCard
